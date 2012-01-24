96e6e2a17d3894ba4bba4ac880adb80b43fb7805
jcrada <jcrada@gmail.com>
2012-01-24 23:10:35 +1300

    TeX Examples and presentation

---
+++ tex/ieee/figures/c_pngtoeps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+convert -quality 100 sphere-0.png sphere-0.eps
+convert -quality 100 sphere-1.png sphere-1.eps
+convert -quality 100 sphere-2.png sphere-2.eps
+convert -quality 100 sphere-3.png sphere-3.eps
+convert -quality 100 sphere-4.png sphere-4.eps
+convert -quality 100 sphere-5.png sphere-5.eps
+
+convert -quality 100 spso-0.png spso-0.eps
+convert -quality 100 spso-1.png spso-1.eps
+
+convert -quality 100 apso-0.png apso-0.eps
+convert -quality 100 apso-1.png apso-1.eps
+convert -quality 100 apso-2.png apso-2.eps
+convert -quality 100 apso-3.png apso-3.eps
+
