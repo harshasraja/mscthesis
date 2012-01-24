bf5129d8e95737beb85cfeedca1b7b0aef0dceb3
jcrada <jcrada@gmail.com>
2012-01-24 21:36:11 +1300

    Experiments added

--- ResearchTestApplication/src/harsha/thesis/exp/Experiment.java
+++ ResearchTestApplication/src/harsha/thesis/exp/Experiment.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package exp;
+package harsha.thesis.exp;
 
 import java.io.BufferedWriter;
 import java.io.File;
--- ResearchTestApplication/src/harsha/thesis/exp/Main.java
+++ ResearchTestApplication/src/harsha/thesis/exp/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package exp;
+package harsha.thesis.exp;
 
 import java.util.Random;
 
