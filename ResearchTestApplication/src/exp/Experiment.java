804c796647f790829317bb519bc79ce11ee1f88c
jcrada <jcrada@gmail.com>
2012-01-24 21:34:42 +1300

    Experiments

---
+++ ResearchTestApplication/src/exp/Experiment.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exp;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+/**
+ *
+ * @author jcrada
+ */
+public class Experiment {
+
+    private String code = "";
+    private String description = "";
+    private String logPath = ".";
+    private long startTime;
+    private long stopTime;
+    private Writer logWriter;
+
+    public Experiment() {
+    }
+
+    public Experiment(String code, String description) {
+    }
+
+    public Experiment(String code, String description, String logPath) {
+        this.code = code;
+        this.description = description;
+        this.logPath = logPath;
+    }
+
+    public void initialize() throws Exception{
+        File file = new File(logPath + "/" + code + ".log");
+        if (file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
+        logWriter = new BufferedWriter(new FileWriter(file));
+    }
+    
+    public void start() throws Exception {
+        setStartTime(System.currentTimeMillis());
+    }
+
+    public void stop() throws Exception {
+        setStopTime(System.currentTimeMillis());
+    }
+    
+    public long ellapsedTime(){
+        return System.currentTimeMillis() - getStartTime();
+    }
+    
+    public long duration(){
+        return getStopTime() - getStartTime();
+    }
+    
+    public void destroy() throws Exception{
+        logWriter.flush();
+        logWriter.close();
+    }
+
+    public void log(String info) throws Exception {
+        logWriter.write(info);
+    }
+
+    public void logf(String info) throws Exception {
+        log(info);
+        logWriter.flush();
+    }
+
+    public Writer writer() {
+        return logWriter;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogPath() {
+        return logPath;
+    }
+
+    public void setLogPath(String logPath) {
+        this.logPath = logPath;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(long stopTime) {
+        this.stopTime = stopTime;
+    }
+}
---
+++ ResearchTestApplication/src/exp/Main.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exp;
+
+import java.util.Random;
+
+/**
+ *
+ * @author jcrada
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        Experiment e = new Experiment("Solution0", "", "/tmp");
+        e.initialize();
+
+        String allArgs = "";
+        for (String s : args) {
+            allArgs += s + " ";
+        }
+
+        e.logf("#" + allArgs + "\n\n");
+
+        Pool.Instance().register(e);
+        for (int i = 0; i < 50; ++i) {
+            test(args);
+        }
+        e.destroy();
+    }
+
+    public static void test(String[] args) throws Exception {
+        Random random = new Random();
+
+        Experiment e = Pool.Instance().get("Solution0");
+
+
+
+        { //Insert
+            e.start();
+            for (int i = 0; i < 100; ++i) {
+//            e.logf("#Inserting " + i + "\n");
+            }
+            Thread.sleep(random.nextInt(500));
+            e.stop();
+
+            e.logf("Insert: " + (e.duration()) + "\n");
+        }
+
+        { //Update
+            e.start();
+            for (int i = 0; i < 100; ++i) {
+//            e.logf("#Inserting " + i + "\n");
+            }
+            Thread.sleep(random.nextInt(500));
+            e.stop();
+
+            e.logf("Update: " + (e.duration()) + "\n");
+        }
+
+        {//Delete
+            e.start();
+            for (int i = 0; i < 100; ++i) {
+//            e.logf("#Inserting " + i + "\n");
+            }
+            Thread.sleep(random.nextInt(500));
+            e.stop();
+
+            e.logf("Delete: " + e.duration() + "\n");
+        }
+        e.logf("\n");
+    }
+}
